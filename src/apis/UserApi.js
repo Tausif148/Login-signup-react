9942880e58a75d188f0ece9e518133a781b292a7
“Tausif” <sheikhtousif148@gmail.com>
2026-03-07 12:39:35 +0530

    added updated code

--- src/apis/UserApi.js
+++ src/apis/UserApi.js
@@ -1,1 +1,5 @@
-    
+const userSignup = ({ email, password }) => {
+    let users = localStorage.getItem('users') || '[]';
+
+    users = JSON.parse(users); // convert string into array or object 
+}
